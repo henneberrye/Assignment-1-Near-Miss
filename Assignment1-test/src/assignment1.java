9d4219d00f0d2832dc361cddcad8ec26afdaa8ef
KBowler1 <122831413+KBowler1@users.noreply.github.com>
2023-02-02 18:26:55 -0600

    Update assignment1.java
    Added opening comment

--- Assignment1-test/src/assignment1.java
+++ Assignment1-test/src/assignment1.java
@@ -1,8 +1,12 @@
 /*
 Fermats Last Therom Near Miss Automated Checker
-Assignment-1-Near-Miss.java
-    
-
+Assignment-1-Near-Miss.java 
+Benjamin Gorgan - benjaminrgorgan@lewisu.edu
+Eugene Henneberry - eugenethenneberry@lewisu.edu
+Kyle Bowler - KyleBowler@lewisu.edu
+CPSC-44000-Software Engineering - Spring '23
+5 Feb 2023
+This program takes a users input and calaculates near misses for the formula Xn + Yn = Zn.
 */
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
