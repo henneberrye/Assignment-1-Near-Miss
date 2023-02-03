e72bc427ed62fd8266715308428008bdaafc358d
KBowler1 <122831413+KBowler1@users.noreply.github.com>
2023-02-02 18:27:24 -0600

    Update assignment1.java

--- Assignment1-test/src/assignment1.java
+++ Assignment1-test/src/assignment1.java
@@ -6,7 +6,7 @@ Eugene Henneberry - eugenethenneberry@lewisu.edu
 Kyle Bowler - KyleBowler@lewisu.edu
 CPSC-44000-Software Engineering - Spring '23
 5 Feb 2023
-This program takes a users input and calaculates near misses for the formula Xn + Yn = Zn.
+This program takes a users input and calculates near misses for the formula Xn + Yn = Zn.
 */
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
