a788320f3b00659dd9377047bb4d7313536a3a8b
bengorg <benjamingorgan@gmail.com>
2023-02-02 18:15:41 -0600

    added while loops for inputs

--- Assignment1-test/src/Input.java
+++ Assignment1-test/src/Input.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 
 public class Input {
     public static void userInput() throws InterruptedException {
-
+        int k = 0;
+        int n = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Please enter a value for k: ");
-        int k = sc.nextInt();
-        System.out.print("Please enter a value for n: ");
-        int n = sc.nextInt();
+        while (k < 10) {
+            System.out.print("Please enter a value for k, greater than or equal to 10: ");
+            k = sc.nextInt();
+        }
+
+        while (n < 2 && n < 12) {
+            System.out.print("Please enter a value for n greater than 2 and less than 12: ");
+            n = sc.nextInt();
+        }
+
         Find.findNearMisses(k, n);
     }
 }
-
-
